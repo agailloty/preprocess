53ebd87abd8284bd6cc5e74e49502a4810cf8185
agailloty <mr.clerisaxel@hotmail.fr>
2025-05-08 16:04:06 +0200

    add new flags to run

--- cmd/run.go
+++ cmd/run.go
@@ -9,25 +9,57 @@ import (
 )
 
 var prepfilePath string
+var datasetPath string
+var columnList []string
+var operationList []string
+var numerics bool
 
 func init() {
 	rootCmd.AddCommand(runCmd)
-
+	runCmd.Run = run
 	runCmd.Flags().StringVarP(&prepfilePath, "file", "f", "Prepfile.toml", "Path to the configuration file")
+	runCmd.Flags().StringVarP(&datasetPath, "data", "d", "", "Path to the dataset file")
+	runCmd.Flags().StringArrayVar(&columnList, "column", []string{}, "Target column(s) for preprocessing")
+	runCmd.Flags().StringArrayVar(&operationList, "op", []string{}, "Preprocessing operation(s) (e.g., fillna:method=mean)")
+	runCmd.Flags().BoolVar(&numerics, "numerics", false, "Apply operations only on numeric columns")
 }
 
 var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Run operations using Prepfile",
-	Run:   run,
 }
 
 func run(cmd *cobra.Command, args []string) {
-	prepfile, err := config.LoadConfig(prepfilePath)
-	if err != nil {
-		fmt.Printf("Failed to load config file '%s': %s\n", prepfilePath, err)
-		return
+	validateFlags()
+}
+
+func validateFlags() {
+	isFileProvided := runCmd.Flags().Changed("file")
+	isDataProvided := runCmd.Flags().Changed("data")
+	isColumnListProvided := runCmd.Flags().Changed("column")
+	isOperationListProvided := runCmd.Flags().Changed("op")
+
+	providedNFlags := btoi(isFileProvided) +
+		btoi(isDataProvided) +
+		btoi(isColumnListProvided) +
+		btoi(isOperationListProvided)
+
+	// if no flag is provided or only --file, then run using Prepfile
+	if providedNFlags == 0 || isFileProvided {
+		prepfile, err := config.LoadConfigFromPrepfile(prepfilePath)
+		if err != nil {
+			fmt.Printf("Failed to load config file '%s': %s\n", prepfilePath, err)
+			return
+		}
+
+		operations.DispatchOperations(prepfile)
 	}
 
-	operations.DispatchOperations(prepfile)
+}
+
+func btoi(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
 }
